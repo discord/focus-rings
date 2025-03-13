9ebc4cf81709ccbb2e73e019bc762354e3acae43
Joe Mun <joe.mun@discordapp.com>
2025-03-12 17:01:23 -0700

    Don't bundle react/jsx-runtime into dist output

--- packages/react-focus-rings/vite.config.ts
+++ packages/react-focus-rings/vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ["react"],
+      external: ["react", "react/jsx-runtime"],
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
